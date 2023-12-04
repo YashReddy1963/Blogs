cfb3487911230d88f99172055185de3efc4eab56
Yash Reddy <119090589+YashReddy1963@users.noreply.github.com>
2023-12-04 21:05:15 +0530

    updated files

---
+++ style.js
@@ -0,0 +1,7 @@
+function opennav(){
+    document.getElementsByClassName("navbar").style.width="100%";
+}
+
+function closenav(){
+    document.getElementsByClassName("navbar").style.width= "0%";
+}
