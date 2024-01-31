858b4b638eac60058c9055768b0b687565abeef0
Yash Reddy <yashmunurreddy63@gmail.com>
2024-01-31 08:36:11 +0530

    update the tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: [
+    './index.html',
+    './project.html',
+    './blog.html'
+  ],
   theme: {
     extend: {
       colors:{
