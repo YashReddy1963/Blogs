c5c5953d3e28224d3ae3ae5c546b0149ae0c89be
Yash Reddy <119090589+YashReddy1963@users.noreply.github.com>
2024-02-20 07:45:39 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,10 @@
+//Navigation bar opening 
+function openNav(){
+    document.getElementById("sideNav").style.width = "100%";
+
+}
+
+//Navigation bar closing
+function closeNav(){
+    document.getElementById("sideNav").style.width = "0%";
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './project.html',
+    './blog.html'
+  ],
+  theme: {
+    extend: {
+      colors:{
+      }
+    },
+  },
+  plugins: [],
+}
+
